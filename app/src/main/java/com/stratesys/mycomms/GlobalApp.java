c64a4a1570cdb16fe68831b975c4f977e05765ee
rubenbujalance <ruben_bujalance@stratesys-ts.com>
2015-04-02 18:33:15 +0200

    Login Screens 3 - Nav Bar

---
+++ app/src/main/java/com/stratesys/mycomms/GlobalApp.java
@@ -0,0 +1,11 @@
+package com.stratesys.mycomms;
+
+import android.app.Application;
+
+/**
+ * Created by str_rbm on 02/04/2015.
+ */
+
+public class GlobalApp extends Application {
+
+}
---
+++ app/src/main/java/com/stratesys/mycomms/LoginSignupActivity.java
@@ -0,0 +1,51 @@
+package com.stratesys.mycomms;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class LoginSignupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_singup_choose);
+
+        Button btSignup = (Button)findViewById(R.id.btSignupMail);
+        btSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(LoginSignupActivity.this, SignupNameActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login_signup, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

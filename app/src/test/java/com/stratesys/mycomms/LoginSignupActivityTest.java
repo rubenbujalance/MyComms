e437b85f5ab0d9dbc03ed598ddfbf873b849f2eb
rubenbujalance <ruben_bujalance@stratesys-ts.com>
2015-04-09 10:46:52 +0200

    First basic Unit Test

---
+++ app/src/test/java/com/stratesys/mycomms/LoginSignupActivityTest.java
@@ -0,0 +1,10 @@
+package com.stratesys.mycomms;
+
+import junit.framework.TestCase;
+
+public class LoginSignupActivityTest extends TestCase {
+
+    public void testOnCreate() throws Exception {
+        assertTrue(true);
+    }
+}

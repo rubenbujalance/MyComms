23d379f77258f449928a7185c0b5e261e0138545
rubenbujalance <ruben_bujalance@stratesys-ts.com>
2015-06-23 10:59:42 +0200

    Chat improvements

--- app/src/main/java/com/vodafone/mycomms/MycommsApp.java
+++ app/src/main/java/com/vodafone/mycomms/MycommsApp.java
@@ -20,6 +20,7 @@ import com.vodafone.mycomms.settings.connection.IProfileConnectionCallback;
 import com.vodafone.mycomms.util.Constants;
 import com.vodafone.mycomms.util.UserSecurity;
 import com.vodafone.mycomms.util.Utils;
+import com.vodafone.mycomms.xmpp.XMPPTransactions;
 
 import java.io.File;
 
@@ -110,6 +111,8 @@ public class MycommsApp extends Application implements IProfileConnectionCallbac
                 userProfile.getFirstName() + " " + userProfile.getLastName(),
                 userProfile.getPlatforms());
         new DownloadContactsAsyncTask().execute(mContext);
+
+        XMPPTransactions.initializeMsgServerSession(mContext, false);
 
         if(sp.getBoolean(Constants.FIRST_TIME_AVATAR_DELIVERY,false))
         {

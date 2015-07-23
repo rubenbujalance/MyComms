449cfae8fd321156a6dcc43cfc592889ac035109
rubenbujalance <ruben_bujalance@stratesys-ts.com>
2015-07-22 19:38:26 -0700

    Open app when clicking a notification

--- app/src/main/java/com/vodafone/mycomms/services/MyGcmListenerService.java
+++ app/src/main/java/com/vodafone/mycomms/services/MyGcmListenerService.java
@@ -1,13 +1,17 @@
 package com.vodafone.mycomms.services;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
 import com.vodafone.mycomms.R;
+import com.vodafone.mycomms.main.DashBoardActivity;
 import com.vodafone.mycomms.util.Constants;
 
 import java.util.Calendar;
@@ -19,6 +23,7 @@ public class MyGcmListenerService extends GcmListenerService {
     @Override
     public void onMessageReceived(String from, Bundle data) {
         String message = data.getString("message");
+        Bundle test = data;
         Log.i(Constants.TAG, "MyGcmListenerService.onMessageReceived: From-"+from+"; Message-"+message);
 
         sendNotification(message);
@@ -31,41 +36,25 @@ public class MyGcmListenerService extends GcmListenerService {
                         .setContentTitle(getString(R.string.app_name))
                         .setContentText(message);
 
-//        PendingIntent resultPendingIntent = null;
-//
-//        if(Build.VERSION.SDK_INT >= 16) {
-//            // The stack builder object will contain an artificial back stack for the
-//            // started Activity.
-//            // This ensures that navigating backward from the Activity leads out of
-//            // your application to the Home screen.
-//            Intent resultIntent = new Intent(this, DashBoardActivity.class);
-//            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-//            // Adds the back stack for the Intent (but not the Intent itself)
-//            stackBuilder.addParentStack(DashBoardActivity.class);
-//            // Adds the Intent that starts the Activity to the top of the stack
-//            stackBuilder.addNextIntent(resultIntent);
-//            resultPendingIntent =
-//                    stackBuilder.getPendingIntent(
-//                            0,
-//                            PendingIntent.FLAG_UPDATE_CURRENT
-//                    );
-//        }
-//        else {
-//            Intent resultIntent = new Intent(this, DashBoardActivity.class);
-//            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-//            // Adds the back stack for the Intent (but not the Intent itself)
-//            stackBuilder.addParentStack(DashBoardActivity.class);
-//            // Adds the Intent that starts the Activity to the top of the stack
-//            stackBuilder.addNextIntent(resultIntent);
-//            resultPendingIntent =
-//                    stackBuilder.getPendingIntent(
-//                            0,
-//                            PendingIntent.FLAG_UPDATE_CURRENT
-//                    );
-//        }
-//
-//        if(resultPendingIntent!=null)
-//            mBuilder.setContentIntent(resultPendingIntent);
+        Intent resultIntent = new Intent(this, DashBoardActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(DashBoardActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        if(resultPendingIntent!=null)
+            mBuilder.setContentIntent(resultPendingIntent);
 
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);

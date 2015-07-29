7199f614a4606540c23edd44e80b3e2613700e35
Albert Mialet <albert.mialet@basetis.com>
2015-07-29 15:54:40 +0200

    New Toolbar butons added Some renaming New background for Contacts and Settings Toolbar

--- app/src/main/java/com/vodafone/mycomms/view/tab/SlidingTabLayoutPreferences.java
+++ app/src/main/java/com/vodafone/mycomms/view/tab/SlidingTabLayoutPreferences.java
@@ -223,7 +223,7 @@ public class SlidingTabLayoutPreferences extends HorizontalScrollView {
 
         if (auxactivity[0].equals("com.vodafone.mycomms.ContactListMainActivity")) {
 
-            /*View tabView = LayoutInflater.from(getContext()).inflate(R.layout.tab_con_favourites, mTabStrip, false);
+            /*View tabView = LayoutInflater.from(getContext()).inflate(R.layout.tab_con_favorites, mTabStrip, false);
             tabView.setOnClickListener(tabClickListener);
             mTabStrip.addView(tabView);
             tabView.getLayoutParams().width = width / 3;

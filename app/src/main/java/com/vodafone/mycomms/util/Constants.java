8ec2e23377cd77d1e0dfbc0d023ebe14e75bd043
Albert Mialet <albert_mialet@stratesys-ts.com>
2015-09-07 16:05:11 +0200

    Error fix

--- app/src/main/java/com/vodafone/mycomms/util/Constants.java
+++ app/src/main/java/com/vodafone/mycomms/util/Constants.java
@@ -320,5 +320,5 @@ public class Constants {
     public static final String DASH_BOARD_ACTIVITY = "DashBoardActivity";
     public static final String MY_COMMS_APP = "MycommsApp";
 
-    public static final int IDLE_TIME = 900000;//90 seconds
+    public static final int IDLE_TIME = 90000;//90 seconds
 }

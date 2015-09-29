d115fa5a551b420e9abb72e399c78d964650e5be
rubenbujalance <ruben_bujalance@stratesys-ts.com>
2015-09-29 09:56:58 +0200

    ChatList Unit Tests

--- app/src/test/java/com/vodafone/mycomms/chat/ChatListActivityTest.java
+++ app/src/test/java/com/vodafone/mycomms/chat/ChatListActivityTest.java
@@ -3,11 +3,22 @@ package com.vodafone.mycomms.chat;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.github.pwittchen.networkevents.library.ConnectivityStatus;
+import com.github.pwittchen.networkevents.library.event.ConnectivityChanged;
 import com.vodafone.mycomms.BuildConfig;
 import com.vodafone.mycomms.R;
 import com.vodafone.mycomms.chatlist.view.ChatListActivity;
+import com.vodafone.mycomms.chatlist.view.ChatListHolder;
+import com.vodafone.mycomms.events.BusProvider;
+import com.vodafone.mycomms.events.ChatsReceivedEvent;
 import com.vodafone.mycomms.realm.RealmChatTransactions;
 import com.vodafone.mycomms.realm.RealmGroupChatTransactions;
 import com.vodafone.mycomms.test.util.MockDataForTests;
@@ -31,6 +42,7 @@ import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowIntent;
+import org.robolectric.shadows.ShadowViewGroup;
 
 import io.realm.Realm;
 
@@ -48,6 +60,9 @@ public class ChatListActivityTest {
     public PowerMockRule rule = new PowerMockRule();
 
     Activity activity;
+    RealmChatTransactions mockChatTx;
+    RealmGroupChatTransactions mockGroupChatTx;
+    RecyclerView recyclerView;
 
     @Before
     public void setUp()
@@ -57,12 +72,12 @@ public class ChatListActivityTest {
         PowerMockito.mockStatic(Realm.class);
 
         //Mock Chat and GroupChat Transactions
-        RealmChatTransactions mockChatTx = PowerMockito.mock(RealmChatTransactions.class);
-        RealmGroupChatTransactions mockGroupChatTx = PowerMockito.mock(RealmGroupChatTransactions.class);
+        mockChatTx = PowerMockito.mock(RealmChatTransactions.class);
+        mockGroupChatTx = PowerMockito.mock(RealmGroupChatTransactions.class);
         PowerMockito.when(mockGroupChatTx.getAllGroupChats(Mockito.any(Realm.class)))
-                .thenReturn(MockDataForTests.getMockGroupChatList());
+                .thenReturn(MockDataForTests.getEmptyGroupChatList());
         PowerMockito.when(mockChatTx.getAllChatsFromExistingContacts(Mockito.any(Realm.class)))
-                .thenReturn(MockDataForTests.getMockChatList());
+                .thenReturn(MockDataForTests.getEmptyChatList());
 
         PowerMockito.mockStatic(RealmGroupChatTransactions.class);
         PowerMockito.when(RealmGroupChatTransactions.getInstance(Mockito.any(Context.class), Mockito.any(String.class)))
@@ -72,6 +87,9 @@ public class ChatListActivityTest {
                 .thenReturn(mockChatTx);
 
         activity = Robolectric.setupActivity(ChatListActivity.class);
+        recyclerView = (RecyclerView)activity.findViewById(R.id.recycler_view);
+        recyclerView.measure(0, 0);
+        recyclerView.layout(0, 0, 100, 10000);
     }
 
     @After
@@ -83,12 +101,80 @@ public class ChatListActivityTest {
         } catch (Exception e) {}
 
         activity = null;
-
+        mockChatTx = null;
+        mockGroupChatTx = null;
+        recyclerView = null;
         System.gc();
     }
 
     @Test
-    public void testCreateGroupClicked()
+    public void testCreateActivity()
+    {
+        FrameLayout fragmentContent = (FrameLayout)activity.findViewById(R.id.sample_content_fragment);
+        Assert.assertNotNull(fragmentContent);
+
+        RelativeLayout footer = (RelativeLayout)activity.findViewById(R.id.footer);
+        Assert.assertNotNull(footer);
+
+        Toolbar toolbar = (Toolbar)activity.findViewById(R.id.app_inbox);
+        Assert.assertNotNull(toolbar);
+
+        RecyclerView recyclerView = (RecyclerView)activity.findViewById(R.id.recycler_view);
+        Assert.assertNotNull(recyclerView);
+    }
+
+    @Test
+    public void testLoadSomeChats()
+    {
+        PowerMockito.when(mockGroupChatTx.getAllGroupChats(Mockito.any(Realm.class)))
+                .thenReturn(MockDataForTests.getMockGroupChatList());
+        PowerMockito.when(mockChatTx.getAllChatsFromExistingContacts(Mockito.any(Realm.class)))
+                .thenReturn(MockDataForTests.getMockChatList());
+
+        //Check number of chats
+        BusProvider.getInstance().post(new ChatsReceivedEvent());
+        Assert.assertTrue(recyclerView.getAdapter().getItemCount() == 8);
+
+        //Check order
+        ChatListHolder holder;
+        holder = (ChatListHolder)recyclerView.findViewHolderForPosition(1);
+        Assert.assertTrue(holder.textViewMessage.toString().compareTo("groupchat_message_1")==0);
+        holder = (ChatListHolder)recyclerView.findViewHolderForPosition(2);
+        Assert.assertTrue(holder.textViewMessage.toString().compareTo("chat_message_1")==0);
+        holder = (ChatListHolder)recyclerView.findViewHolderForPosition(3);
+        Assert.assertTrue(holder.textViewMessage.toString().compareTo("groupchat_message_2")==0);
+        holder = (ChatListHolder)recyclerView.findViewHolderForPosition(4);
+        Assert.assertTrue(holder.textViewMessage.toString().compareTo("chat_message_2")==0);
+        holder = (ChatListHolder)recyclerView.findViewHolderForPosition(5);
+        Assert.assertTrue(holder.textViewMessage.toString().compareTo("groupchat_message_3")==0);
+        holder = (ChatListHolder)recyclerView.findViewHolderForPosition(6);
+        Assert.assertTrue(holder.textViewMessage.toString().compareTo("chat_message_3")==0);
+        holder = (ChatListHolder)recyclerView.findViewHolderForPosition(7);
+        Assert.assertTrue(holder.textViewMessage.toString().compareTo("groupchat_message_4")==0);
+        holder = (ChatListHolder)recyclerView.findViewHolderForPosition(8);
+        Assert.assertTrue(holder.textViewMessage.toString().compareTo("chat_message_4")==0);
+
+        //Check data of 1st and 2nd position
+        ChatListHolder firstPos = (ChatListHolder)recyclerView.findViewHolderForPosition(1);
+        Assert.assertTrue(firstPos.top_left_avatar.getVisibility()==View.VISIBLE);
+        Assert.assertTrue(firstPos.top_right_avatar.getVisibility()==View.VISIBLE);
+        Assert.assertTrue(firstPos.bottom_left_avatar.getVisibility()==View.VISIBLE);
+        Assert.assertTrue(firstPos.bottom_right_avatar.getVisibility()==View.GONE);
+        Assert.assertTrue(firstPos.top_left_chat_availability.getVisibility()==View.VISIBLE);
+        Assert.assertTrue(firstPos.top_right_chat_availability.getVisibility()==View.VISIBLE);
+        Assert.assertTrue(firstPos.bottom_left_chat_availability.getVisibility()==View.VISIBLE);
+        Assert.assertTrue(firstPos.bottom_right_chat_availability.getVisibility()==View.GONE);
+
+        //Check scroll
+        ShadowViewGroup shadowRecycler = Shadows.shadowOf(recyclerView);
+        shadowRecycler.scrollTo(5000, 0);
+        recyclerView.scrollToPosition(7);
+
+        //Check click
+    }
+
+    @Test
+    public void testCreateGroupChatClicked()
     {
         ImageView btCreateGroup = (ImageView)activity.findViewById(R.id.chat_add);
         btCreateGroup.performClick();
@@ -99,16 +185,19 @@ public class ChatListActivityTest {
         Assert.assertTrue(shadowIntent.getComponent().getClassName().compareTo(ChatListActivity.class.getName())!=0);
     }
 
-//    @Test
-//    public void testConnectivityChanged()
-//    {
-//        LinearLayout lay_no_connection = (LinearLayout)activity.findViewById(R.id.no_connection_layout);
-//        ConnectivityChanged event = new ConnectivityChanged();
-//        BusProvider.getInstance().post();
-//
-//        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
-//        Intent startedIntent = shadowActivity.getNextStartedActivity();
-//        ShadowIntent shadowIntent = Shadows.shadowOf(startedIntent);
-//        Assert.assertTrue(shadowIntent.getComponent().getClassName().compareTo(ChatListActivity.class.getName())!=0);
-//    }
+    @Test
+    public void testConnectivityChanged()
+    {
+        LinearLayout lay_no_connection = (LinearLayout)activity.findViewById(R.id.no_connection_layout);
+        ConnectivityChanged eventDisconnected = new ConnectivityChanged(ConnectivityStatus.OFFLINE);
+        ConnectivityChanged eventConnected = new ConnectivityChanged(ConnectivityStatus.MOBILE_CONNECTED);
+
+        //Trying disconnect
+        BusProvider.getInstance().post(eventDisconnected);
+        Assert.assertTrue(lay_no_connection.getVisibility() == View.VISIBLE);
+
+        //Trying connect
+        BusProvider.getInstance().post(eventConnected);
+        Assert.assertTrue(lay_no_connection.getVisibility()== View.GONE);
+    }
 }
